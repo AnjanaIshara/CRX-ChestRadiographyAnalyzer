da0ff880cba6727f034586cd556e789e08be7f5f
anjana ishara <anjanaishara08@gmail.com>
2021-02-03 13:06:00 +0530

    IMAGE UPLOAD BUTTON SET

--- views/PredictionScreen.js
+++ views/PredictionScreen.js
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import { Button,View, Text,TouchableOpacity } from 'react-native';
+import { Button,View, Text,Image,TouchableOpacity } from 'react-native';
+
 const PredicitonsScreen=({navigation})=>{
+    
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Welcome to the Predictions srcreen</Text>
      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
          <Text>Go to Home</Text>
      </TouchableOpacity>
-     
+        <TouchableOpacity>
+            <Text style={{color:'#A000FF'}}>
+                Upload the image
+            </Text>
+        </TouchableOpacity>
       
     </View>
     )
